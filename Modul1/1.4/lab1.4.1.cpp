cd7a2639e2403ef8cc55fbbd59685abd1e123d63
yevheniiamotorna <yevheniiamotorna@users.noreply.github.com>
2017-10-23 12:42:31 +0300

    'origin/master'

--- Modul1/1.4/lab1.4.1.cpp
+++ Modul1/1.4/lab1.4.1.cpp
@@ -3,14 +3,15 @@
 #include <string>
 int main() {
   float v=2;
-  loat result = v + 1 * 2;
+  float result = (v + 1) * 2;
   std::cout << "result:  " << result <<  "  expected result :  6" << std::endl;
-  result = v + 1 * v + 2 * 2;
+  result = (v + 1) * (v + 2) * 2;
   std::cout << "result: " << result <<  "  expected result : 24" << std::endl;
-  result = v - 1 * 2 + 2 * 2;
+  result = (v - 1) * 2 + 2 * 2;
   std::cout << "result:  " << result <<  "  expected result :  6" << std::endl;
-  result = v + v * v + v * 2;
+  result = (v + v) * (v + v) * 2;
   std::cout << "result: " << result <<  "  expected result : 32" << std::endl;
-  result = (int)v % 2 * v + 2 * 2;
+  result = (int)v % 2 * (v + 2 * 2);
   std::cout << "result:  " << result <<  "  expected result :  0" << std::endl;
+  return 0;
  }
