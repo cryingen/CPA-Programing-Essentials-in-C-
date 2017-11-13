8d5dafe803dce524bd5d6256d441f69dd1bb3d19
yevheniiamotorna <yevheniiamotorna@users.noreply.github.com>
2017-11-13 23:48:46 +0200

    laaaaaabs

---
+++ Modul4/4.5/lab4.5.7(2).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string text, from, to;
+    cout << "Input original text: ";
+    getline(cin, text);
+    cout << "Input the word you want to replace: ";
+    getline(cin, from);
+    cout << "Input the wrod you want to replace with: ";
+    getline(cin, to);
+    int pos = text.find(from);
+    while (pos != -1)
+    { 
+        text.replace(pos, from.size(), to);
+        pos = text.find(from);
+    }
+    cout << text << endl;
+    return 0;
+}
